efb8b2875e81b44e3d9f2cfa9b25b9ce1163be96
Milla Voilokova <milla.voilokova@gmail.com>
2024-01-18 19:06:37 -0500

    process UID from form logic

--- service/process-uids.go
+++ service/process-uids.go
@@ -6,26 +6,31 @@ import (
 )
 
 func processXandrUID(str string) []string {
-
+	var err error
 	var xuid int64
 	var xuidErrList []string
 	xuidErrList = nil
 
 	negative := "Negative number"
-	//zero := "cannot start from 0"
+	letters := "ID must contain numbers only"
+	zero := "cannot start from 0"
 
 	log.Println("str[0]: ", string(str[0]))
 
-	xuid, _ = strconv.ParseInt(str, 10, 64)
-	if xuid == 0 {
-		return xuidErrList
+	xuid, err = strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		log.Println("xuid parcing err: ", err)
+		xuidErrList = append(xuidErrList, letters)
+	} else if string(str[0]) == "0" {
+		log.Println(zero)
+	} else if xuid == 0 {
 		log.Println("empty the Xanrd userID field")
+	} else {
+		log.Println("xuid:", xuid)
+		if xuid <= 0 {
+			xuidErrList = append(xuidErrList, negative)
+		}
 	}
-	log.Println("xuid:", xuid)
-	if xuid <= 0 {
-		xuidErrList = append(xuidErrList, negative)
-	}
-
 	for i := 0; i < len(xuidErrList); i++ {
 		log.Println(i, ". ", xuidErrList[i])
 	}
