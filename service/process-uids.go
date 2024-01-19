1c684bc04b8bb47e8b142e402e309cd34e76cdaf
Milla Voilokova <milla.voilokova@gmail.com>
2024-01-19 11:21:00 -0500

    id validator finished

--- service/process-uids.go
+++ service/process-uids.go
@@ -11,12 +11,14 @@ func processXandrUID(str string) xandr {
 	var xuid int64
 	var xr xandr
 	xr.ErrList = nil
+	xr.UserID = 0
 
-	negative := "Negative number"
+	negative := "ID cannot be negative"
 	letters := "ID must contain numbers only"
 	zero := "Cannot start from 0"
 	large := "UserID value is out of range"
 	empty := "Empty the Xanrd userID field"
+	valid := "ID validation completed. No errors or warnings founded."
 
 	log.Println("str[0]: ", string(str[0]))
 
@@ -25,26 +27,30 @@ func processXandrUID(str string) xandr {
 		if strings.Contains(err.Error(), "out of range") {
 			xr.ErrList = append(xr.ErrList, large)
 			log.Println(large)
+			xr.WrongUserID = str
 		}
 		if strings.Contains(err.Error(), "invalid syntax") {
 			xr.ErrList = append(xr.ErrList, letters)
 			log.Println(letters)
+			xr.WrongUserID = str
 		} else {
 			log.Println("other err: ", err)
 		}
 	} else if string(str[0]) == "0" {
 		xr.ErrList = append(xr.ErrList, zero)
 		log.Println(zero)
+		xr.WrongUserID = str
 	} else if xuid == 0 {
 		xr.ErrList = append(xr.ErrList, empty)
 		log.Println(empty)
+		xr.WrongUserID = "Empty input"
+	} else if xuid <= 0 {
+		xr.ErrList = append(xr.ErrList, negative)
+		log.Println("negative")
+		xr.WrongUserID = str
 	} else {
-		log.Println("xuid:", xuid)
-		if xuid <= 0 {
-			xr.ErrList = append(xr.ErrList, negative)
-			log.Println("negative")
-		}
-
+		xr.ValidMsg = valid
+		xr.UserID = xuid
 	}
 	for i := 0; i < len(xr.ErrList); i++ {
 		log.Println(i, ". ", xr.ErrList[i])
