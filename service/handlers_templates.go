733b01b702e9de498699cd2acee851988b9631e2
Milla Voilokova <milla.voilokova@gmail.com>
2024-02-28 08:57:58 -0500

    handlers changes

--- service/handlers_templates.go
+++ service/handlers_templates.go
@@ -27,34 +27,27 @@ func handleXandrtools(w http.ResponseWriter, r *http.Request) {
 		ValidationResult xandr
 		Errs             bool
 		ValUUID          uuid
-		SecOne           string
-		SecTwo           string
-		SecThree         string
-		SecFour          string
-		SecFive          string
+		SecOne           string //section one of uuid
+		SecTwo           string //section two of uuid
+		SecThree         string //section three of uuid
+		SecFour          string //section four of uuid
+		SecFive          string //section five of uuid
 	}
 	var d data
 	var err error
 	d.Errs = false
 
-	//1. path = validate and type = xandrid
-	if r.URL.Path == "/validate" && r.URL.Query().Get("type") == "xandrid" {
-		log.Println("VALIDATE TYPE: ", r.URL.Query().Get("type"))
+	//1. input name = "type" value = "xandrid"
+	if r.URL.Query().Get("type") == "xandrid" {
 		id := r.URL.Query().Get("id")
-		log.Println("XandrID = ", id)
 		d.ValidationResult = processXandrUID(id)
-
-		log.Println("len errs: ", len(d.ValidationResult.ErrList))
 		if len(d.ValidationResult.ErrList) > 0 {
 			d.Errs = true
 		}
-		log.Println("errs: ", d.Errs)
 	}
-	//2. path = validate and type = uuid
-	if r.URL.Path == "/validate" && r.URL.Query().Get("type") == "uuid" {
-		log.Println("VALIDATE TYPE: ", r.URL.Query().Get("type"))
+	//2. input name = "type" value = "uuid"
+	if r.URL.Query().Get("type") == "uuid" {
 		id := r.URL.Query().Get("id")
-		log.Println("UUID = ", id)
 		d.ValUUID, err = validateUUID(id)
 		if err != nil {
 			log.Println("ValUUD err: ", len(d.ValUUID.ErrMsg))
@@ -67,7 +60,6 @@ func handleXandrtools(w http.ResponseWriter, r *http.Request) {
 			d.SecFour = d.ValUUID.Sections[3]
 			d.SecFive = d.ValUUID.Sections[4]
 		}
-		log.Println("errmsg: ", d.ValUUID.ErrMsg)
 	}
 
 	if err := t.ExecuteTemplate(w, "xandrtools.html", d); err != nil {
@@ -96,39 +88,29 @@ func handleTextGenerator(w http.ResponseWriter, r *http.Request) {
 	d.ShowText = false
 	var segs []string
 
-	//log.Println("ID = ", d.ID)
-	log.Println("initializing len of segs: ", len(segs))
-
 	d.Seps.Sep1 = r.URL.Query().Get("sep_1")
 	d.Seps.Sep2 = r.URL.Query().Get("sep_2")
 	d.Seps.Sep3 = r.URL.Query().Get("sep_3")
 	d.Seps.Sep4 = r.URL.Query().Get("sep_4")
 	d.Seps.Sep5 = r.URL.Query().Get("sep_5")
 
-	log.Println("1. -----------SET DEFAULT SEPARATORS------------")
-
+	//set default separators
 	setDefaultSeparators(&d.Seps)
-	log.Println("2. -----------CHECK SEPARATORS------------")
+
 	//check separators
 	if err := checkSeparators(d.Seps); err != nil {
 		d.SepError = err.Error()
 	}
-	log.Println("3. -----------CHECK SF------------")
+	//check sf value
 	sf := r.URL.Query().Get("sf")
 	segmentFields := strings.Split(sf, "-")
-	/*if sf != "" {
-		d.ShowText = true
-	}*/
-	log.Println("d.SepError: ", d.SepError)
-	log.Println("4. -----------CHECK SEGMENTS------------")
+
 	// checks segments
 	d.SegError, err = checkSegments(segmentFields)
 	if err != nil {
 		d.SegError = err.Error()
 		log.Println("d.SegError error: ", d.SegError)
 	}
-
-	log.Println("d.SegError = ", d.SegError)
 
 	var js string
 	for _, f := range segmentFields {
@@ -139,10 +121,7 @@ func handleTextGenerator(w http.ResponseWriter, r *http.Request) {
 	}
 	d.InitScript = template.JS(js)
 
-	log.Println("5. -----------GENERATE SAMPLE------------")
-	log.Println("len d.SepError = ", len(d.SepError))
-	log.Println("len of d.SegError", len(d.SegError))
-	log.Println("SEGMENT FIELDS: ", segmentFields)
+	//generate text sample
 	if len(d.SegError) == 0 && sf != "" && len(d.SepError) == 0 {
 		d.ShowText = true
 		d.GeneratedText = generateSample(segmentFields, d.Seps)
