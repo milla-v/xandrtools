b9ac2b813938a093142c2cb22c7bbf2b9f0c6e0d
Ludmila Voilokova <ludmila.voilokova@gmail.com>
2024-04-30 21:43:30 -0400

    Fix restart

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ scp /tmp/xandrtools xandrtools:xandrtools.new
 ssh xandrtools '
 		mv --backup=numbered xandrtools xandrtools.old
 		mv xandrtools.new xandrtools
-		sudo service restart xandrtools
+		sudo service xandrtools restart
 '
