2a2fca7c2cde7628212c06953ee478c16e1a2899
Milla Voilokova <milla.voilokova@gmail.com>
2024-05-27 12:26:57 -0400

    generate batch segment upload jobs in cycle

--- simulator/batchsegment.go
+++ simulator/batchsegment.go
@@ -103,41 +103,6 @@ func generateBatchSegmentUploadJob(numJobs int) ([]BatchSegmentUploadJob, error)
 
 		log.Println("FOR len uploadJob.TimeToProcess: ", len(list))
 	}
-	/*
-		var u BatchSegmentUploadJob
-		u.StartTime = time.Now()
-		u.UploadedTime = u.StartTime.Add(time.Second * 6)
-		u.ValidatedTime = u.UploadedTime.Add(time.Minute * 3)
-		u.CompletedTime = u.ValidatedTime.Add(time.Minute * 1)
-		u.CreatedOn = u.StartTime
-		//u.ErrorCode =
-		u.ErrorLogLines = "\n\nnum_unauth_segment-4013681496264948522;5013:0,5014:1550"
-		u.ID = int64(rand.Uint64())
-		//u.IsBeamFile =
-		u.JobID, err = generateToken(20)
-		if err != nil {
-			log.Println("generate jobId err: ", err)
-			return uploadJob, err
-		}
-		u.LastModified = u.CompletedTime
-		u.MemberID = int32(rand.Uint32())
-		u.NumInactiveSegment = 0
-		u.NumInvalidFormat = 0
-		u.NumInvalidSegment = 0
-		//u.NumInvalidTimestamp =
-		u.NumInvalidUser = 0
-		u.NumOtherError = 0
-		u.NumPastExpiration = 0
-		u.NumUnauthSegment = 1
-		u.NumValid = 200000
-		u.NumValidUser = 100000
-		u.PercentComplete = 100
-		u.Phase = "completed"
-		u.SegmentLogLines = "\n5010:100000\n5011:50000\n5012:50000"
-		u.TimeToProcess = u.CompletedTime.Sub(u.StartTime)
-		uploadJob = append(uploadJob, u)
 
-		log.Println("FOR len uploadJob.TimeToProcess: ", len(uploadJob))
-	*/
 	return list, err
 }
